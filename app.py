99d9acd77bc6e58db5b9e4304df55028d08a192e
Unais Siddiqui <unaisyoushasiddiqui@gmail.com>
2020-11-07 15:33:34 +0500

    initial commit

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 from flask import Flask , render_template ,redirect , url_for,request
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'login'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text)
+    password = db.Column(db.Text) 
+
+
+@app.route('/')
+def index():
+    return render_template("base1.html")
 
 
 
