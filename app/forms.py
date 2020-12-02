d03c117404040ba88e5412e069da5eec4daffb78
Unais Siddiqui <unaisyoushasiddiqui@gmail.com>
2020-12-02 13:29:41 +0500

    Cleaning

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,6 @@ from flask import render_template
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
 class RegistrationForm(FlaskForm):
--- app/routes.py
+++ app/routes.py
@@ -21,8 +21,7 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('home_page'))
+        return render_template('dashboard.html')
     return render_template('login.html', title='Sign In', form=form)
 
 
