6d11bce33c62b9c05202b68d527aed67aab31dbd
alishbah13 <alishbah13@gmail.com>
2020-12-06 23:52:18 +0500

    mod

--- app/email.py
+++ app/email.py
@@ -11,6 +11,7 @@ def send_password_reset_email(user):
                 sender= 'investflycorporation@gmail.com',
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt', user=user, token=token),
+            #    remove text body
                html_body=render_template('email/reset_password.html',user=user, token=token))
 
 def send_user_verification_email(user):
@@ -19,6 +20,7 @@ def send_user_verification_email(user):
                 sender= 'investflycorporation@gmail.com',
                recipients=[user.email],
                text_body=render_template('email/welcome.txt',user=user, token=token),
+            #    remove text body
                html_body=render_template('email/welcome.html',user=user, token=token))
                                          
 def send_async_email(app, msg):
