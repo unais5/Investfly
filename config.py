871c271b5a3dfb781e853ad140433407777fd038
Unais Siddiqui <unaisyoushasiddiqui@gmail.com>
2020-12-23 02:48:24 +0500

    Db Timeout

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import datetime
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    SQLALCHEMY_POOL_RECYCLE = 90
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
