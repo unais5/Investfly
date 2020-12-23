c38f21b6a82c65044183d14a332f529a1f47a18a
Unais Siddiqui <unaisyoushasiddiqui@gmail.com>
2020-12-23 11:22:15 +0500

    changes

--- migrations/versions/7b4fc5e2e84d_new_db.py
+++ migrations/versions/7b4fc5e2e84d_new_db.py
@@ -1,8 +1,8 @@
-"""fk err
+"""New DB
 
-Revision ID: f5879e322a27
+Revision ID: 7b4fc5e2e84d
 Revises: 
-Create Date: 2020-12-23 01:14:30.162330
+Create Date: 2020-12-23 10:27:12.561296
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'f5879e322a27'
+revision = '7b4fc5e2e84d'
 down_revision = None
 branch_labels = None
 depends_on = None
